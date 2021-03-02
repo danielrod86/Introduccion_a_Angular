93c07eb168e476c843555dbf822bbfb37f90fdc5
danielrod86 <danielrod86@gmail.com>
2021-03-02 19:03:18 +0100

    Terminado ejercicio de los Data Driven

--- miapp/src/app/c4/c4.component.ts
+++ miapp/src/app/c4/c4.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from '../persona';
 
 @Component({
   selector: 'app-c4',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class C4Component implements OnInit {
 
+  seleccionada: Persona;
+
+  listaPersonas: Persona[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.listaPersonas.push(new Persona("pedro", "perez", 20), new Persona("ana", "gomez", 30));
+    this.listaPersonas.push(new Persona("juan", "sanchez", 50), new Persona("david", "gomez", 60));
+  }
+
+  seleccionarPersona(persona: Persona): void {
+    this.seleccionada = persona;
+
   }
 
 }
